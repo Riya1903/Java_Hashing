e88a2c6c064df3724b635767177200b6d03d8883
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-06-21 21:28:51 +0530

    Add files via upload

---
+++ HashmapEx.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class HashmapEx {
+public static void main(String[] args) {
+    HashMap<String,Integer>hm= new HashMap<>();
+    //insert
+    hm.put("India", 100);
+    hm.put("China", 150);
+    hm.put("US", 50);
+    System.out.println(hm);
+    //get-O(n)
+    int population =hm.get("India");
+    System.out.println(population);
+    //Containkey-O(n)
+    System.out.println(hm.containsKey("India"));
+    System.out.println(hm.containsKey("Indindonasia"));
+
+    //remove-O(n)
+    System.out.println(hm.remove("US"));
+    System.out.println(hm);
+    
+}
+}
